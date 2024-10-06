2dec81ca511df388469ca03b4f019fafa5a93982
Fiona Kanyi <fionakanyi0@gmail.com>
2024-10-07 00:39:52 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+// Function Declaration for saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function Expression for mondayWork
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function to create a wrapAdjective function
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Example calls
+  console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+  console.log(saturdayFun("go hiking")); // "This Saturday, I want to go hiking!"
+  
+  console.log(mondayWork()); // "This Monday, I will go to the office."
+  console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer")); // "You are !!!a dedicated programmer!!!"
+  console.log(wrapAdjective("%")("a dedicated programmer")); // "You are %a dedicated programmer%!"
+  
